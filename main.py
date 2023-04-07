7d0b105b9601bc608f25f5ff25323dc5b2838331
JAYANTH <jayanthsoppara@gmail.com>
2023-04-07 20:54:55 +0530

    done

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from sensor.configaration.mongo_db_connection import MongoDBClient
 
 if __name__=='__main__':
     mongodb_clinet=MongoDBClient()
+    print("collection names : ",mongodb_clinet.database.list_collection_names())
